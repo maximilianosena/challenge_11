7f95d2f6dad93fa964c0afb36803ed056ab3f0b1
maximilianosena <maximilianosena13@gmail.com>
2023-09-05 11:35:22 -0300

    Subo

---
+++ script.js
@@ -0,0 +1,33 @@
+const URL = 'https://jsonplaceholder.typicode.com/users'
+
+let firstName = document.getElementById("name");
+let lastName = document.getElementById("lastName");
+let birthday = document.getElementById("birthday");
+let btnSubmit = document.getElementById("submit")
+
+const information = {
+    nombre : firstName.value,
+    apellido : lastName.value,
+    nacimiento : birthday.value
+}
+
+const metodo = {
+    method : 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(information),
+};
+
+async function postInformation(URL, metodo){
+    let response = await fetch (URL)
+    if (response.ok) {
+        console.log("Estado: ", response.status)
+
+    } else {
+        alert("Estado: ", response.status)
+    }
+}
+
+btnSubmit.addEventListener("click", (event)=>{
+    event.preventDefault()
+    postInformation(URL, metodo)
+})
