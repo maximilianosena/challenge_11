68d3c4507f218f6c1c51496b51118aa14675249f
maximilianosena <maximilianosena13@gmail.com>
2023-09-05 12:04:07 -0300

    Update
    Para ver la informacion

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ let lastName = document.getElementById("lastName");
 let birthday = document.getElementById("birthday");
 let btnSubmit = document.getElementById("submit")
 
-const information = {
-    nombre : firstName.value,
-    apellido : lastName.value,
-    nacimiento : birthday.value
-}
+h
+async function postInformation(URL){
 
+//Creo el objeto que va a enviarse a la URL
+    const information = {
+        nombre : firstName.value,
+        apellido : lastName.value,
+        nacimiento : birthday.value
+    }
+
+    //Creo el objeto de metodo, y la información que se envía en formato JSON
 const metodo = {
     method : 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(information),
 };
 
-async function postInformation(URL){
+
+//Ingreso el metodo como segundo parametro del fetc
     let response = await fetch (URL, metodo)
     if (response.ok) {
         console.log("Estado: ", response.status)
-
+        console.log("Nombre: ", await response.json())
     } else {
         alert("Estado: ", response.status)
     }
@@ -29,5 +35,5 @@ async function postInformation(URL){
 
 btnSubmit.addEventListener("click", (event)=>{
     event.preventDefault()
-    postInformation(URL, metodo)
+    postInformation(URL)
 })
